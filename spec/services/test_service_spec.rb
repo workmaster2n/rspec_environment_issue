b31b16a4e1b1fe48699e71e218e68b093a2c419c
Tyler DeWitt <tdewitt@peritus.com>
2015-08-12 16:20:52 -0500

    remove unneeded spec

--- spec/services/test_service_spec.rb
+++ spec/services/test_service_spec.rb
@@ -1,11 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe TestService do
-  it 'loads up other classes' do
-    Person.create
-    expect(TestService.new.do).to eq('hi')
-  end
-  it 'can be found' do
+  it 'can be run' do
     expect(TestService.new.do).to eq('hi')
   end
 end
